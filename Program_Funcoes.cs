903fe66b31996397260875538ac5bf611179ba51
Rafael Dantas <34816420+Programador89@users.noreply.github.com>
2022-03-23 18:30:45 -0300

    Aplicando conceito de função ao programa
    Selecionando o maior numero recebido por meio de função(método).

---
+++ Program_Funcoes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+class Program_Funcoes {
+    static void Main(string[] args){
+        // Definir qual é o maior número.   
+
+        System.Console.WriteLine("Digite 3 numeros: ");
+        int numberOne = int.Parse(Console.ReadLine());
+        int numberTwo = int.Parse(Console.ReadLine());
+        int numberTree = int.Parse(Console.ReadLine());
+
+        double higherNumber = Higher(numberOne, numberTwo, numberTree);
+
+        System.Console.WriteLine($"Maior número: {higherNumber}");
+    }
+
+    static int Higher(int firstNumber, int secoundNUmber, int thirdNumber) {
+        int bigNumber = 0;
+
+        if(firstNumber > secoundNUmber && firstNumber > thirdNumber){
+            bigNumber = firstNumber;
+        }else if(secoundNUmber > thirdNumber){
+            bigNumber = secoundNUmber;
+        }else {
+            bigNumber = thirdNumber;
+        }
+        return bigNumber;
+    }
+}
